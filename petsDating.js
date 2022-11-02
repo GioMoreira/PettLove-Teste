acb22b5fc1abe3138f63dbd921aa1a10e6077f02
GioMoreira <sgiomoreira@gmail.com>
2022-11-02 18:50:47 -0300

    loading-more

--- petsDating.js
+++ petsDating.js
@@ -1,14 +1,17 @@
 const image_array = [
     'Abigail.png',
-    'bingo.png'
+    'bingo.png',
+    'Guido.png',
+    'jade.png',
+    'jujuba.png',
+    'kiwi.png',
+    'lennon.png',
+    'pancho.png'
 ]
 
-
-function get_pet() {
- 
-}
 class PetsDating extends HTMLElement {
     connectedCallback() {
+        
         this.innerHTML = `
         <div uk-scrollspy="cls:uk-animation-fade">
         <section class="pets-dating-container">
@@ -16,48 +19,49 @@ class PetsDating extends HTMLElement {
             <div class="couple-column">
                 <div class="couples-pics">
                     <div>
-                        <img id="img1" src="assets/Guido.png" alt="">
+                        <img id="" src="assets/Guido.png" alt="pet1">
                     </div>
         
                     <div>
-                        <img id="img2" src="assets/Abigail.png" alt="">
+                        <img id="pet2" src="assets/Abigail.png" alt="pet2">
                     </div>
                 </div> 
     
-                <h2>Guido e Abigail</h2>
+                <h2 id="">Guido e Abigail</h2>
             </div>
     
             <div class="couple-column">
                 <div class="couples-pics">
                     <div>
-                        <img src="assets/pancho.png" alt="">
+                        <img id="" src="assets/pancho.png" alt="pet1">
                     </div>
         
                     <div>
-                        <img src="assets/kiwi.png" alt="">
+                        <img id="" src="assets/kiwi.png" alt="pet2">
                     </div>
-                </div>
+                </div> 
     
-                <h2>Pancho e Kiwi</h2>
+                <h2 id="">Pancho e Kiwi</h2>
             </div>
     
             <div class="couple-column">
-                <div class="couples-pics">
-                    <div>
-                        <img src="assets/bingo.png" alt="">
-                    </div>
-        
-                    <div>
-                        <img src="assets/jade.png" alt="">
-                    </div>
+            <div class="couples-pics">
+                <div>
+                    <img id="" src="assets/bingo.png" alt="pet1">
                 </div>
     
-                <h2>Bingo e Jade</h2>
+                <div>
+                    <img id="" src="assets/jade.png" alt="pet2">
+                </div>
+            </div> 
+
+            <h2 id="">Bingo e Jade</h2>
+        </div>
             </div>
         </section>
     </div>
     `
-        
+
     }
 }
 
